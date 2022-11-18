dc0862b2d7604f56af70fa02fb9d2a87f7c15d63
Waffle <ethanstorm11@gmail.com>
2022-11-17 19:31:50 -0500

    Fixed issues with JS and HTML, added some CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var quizBeginning = $('quiz-open');
 var quizMain = $('quiz');
 var startQuiz = $('quiz-start');
-var quizTimer = $('#timer');
+var quizTimer = document.getElementById('timer');
 var quizEnd = $('quiz-end');
 var finalScore = $('final-score');
 var scoreInputName = $('name-submission');
@@ -17,13 +17,21 @@ var buttonD = $('button-d');
 // General variables
 var timerInterval
 var timeLeft = 80;
-var score = 0
+var score = 0;
 
+function startQuiz(){
 // Timer functionality
-timerInterval = setInterval(function() {
-    timeLeft--;
-    quizTimer.textContent = "Time left: " + timeLeft;
-});
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        quizTimer.textContent = "Time left: " + timeLeft;
+    });
+}
+
+// Restarts Quiz
+function replayQuiz() {
+    timeLeft = 80;
+    score = 0;
+}
 
 // Button to start quiz
 startQuizButton.addEventListener("click", startQuiz);
